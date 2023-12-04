72a6d9c159bd15ec18823d30029ca19c96833742
Utkarsh <utkarshsingh6207603306@gmail.com>
2023-12-04 11:04:57 +0530

    Committing changes in submodule

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { MONGO_URI } = process.env;
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+  }
+};
